f84d64ba32fb4dc69bbcf294494ce34a93d04c07
Ehsan Taghavi <ehsantgv@gmail.com>
2020-10-25 23:56:34 +0330

    Implement Animating Circle size

--- lib/animated_circle_page.dart
+++ lib/animated_circle_page.dart
@@ -5,24 +5,57 @@ class AnimatedCirclePage extends StatefulWidget {
   _AnimatedCirclePageState createState() => _AnimatedCirclePageState();
 }
 
-class _AnimatedCirclePageState extends State<AnimatedCirclePage> {
+class _AnimatedCirclePageState extends State<AnimatedCirclePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(
+        seconds: 1,
+      ),
+      vsync: this,
+    );
+    animation = Tween(begin: 1.0, end: 2.0).animate(animationController);
+
+    animationController.addStatusListener(animationStatusListener);
+    animationController.forward();
+  }
+
+  void animationStatusListener(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      animationController.reverse();
+    } else if (status == AnimationStatus.dismissed) {
+      animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Animated Circle"),
       ),
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.circular(50),
+      body: AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget child) {
+          final size = 100 * (animation.value);
+          return Center(
+            child: Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(size / 2),
+                ),
+                color: Colors.red,
+              ),
             ),
-            color: Colors.red,
-          ),
-        ),
+          );
+        },
       ),
     );
   }
